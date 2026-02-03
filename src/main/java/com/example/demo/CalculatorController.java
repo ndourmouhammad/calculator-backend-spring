758583f206bbca3476ddb6c3d1365c028b707cda
ndourmouhammad <ndourmouhammad15@gmail.com>
2026-02-03 23:35:48 +0000

    Initial commit: Ajout de la calculatrice simple

---
+++ src/main/java/com/example/demo/CalculatorController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/calculator")
+public class CalculatorController {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.add(a, b);
+    }
+}
